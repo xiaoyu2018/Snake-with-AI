6b37719d8805bb0fb686278ad9ed0710a199e2c6
xiaoyu2018 <861900161@qq.com>
2022-07-15 21:13:51 +0800

    😤

--- src/algorithms/dqn.py
+++ src/algorithms/dqn.py
@@ -5,14 +5,16 @@ import torch
 class LinerNet(nn.Module):
     def __init__(self, input_size, output_size):
         super(LinerNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, 256)
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, output_size)
         self.relu=nn.ReLU()
-        self.fc2 = nn.Linear(256, output_size)
+        
 
     def forward(self, x):
-        x = self.fc1(x)
-        x=self.relu(x)
-        x = self.fc2(x)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 
@@ -30,10 +32,13 @@ class DQN(Model):
         print(out)
         return torch.argmax(out).item()
 
-    def train_step(self, state, reward):
+    def train_step(self, state, action, reward, next_state):
+        state=torch.tensor(state,dtype=torch.float)
+        reward=torch.tensor(reward,dtype=torch.float)
+
         self.optimizer.zero_grad()
-        output = self.net(state)
-        loss = self.loss_func(output, reward)
+        out = self.net(state)
+        loss = self.loss_func(out, reward)
         loss.backward()
         self.optimizer.step()
         return loss.item()
